b9822bfe4be967912acedf548a2a4de8dc0acb75
Freddysotof <freddy.sotof23@hotmail.com>
2019-08-07 19:54:38 -0400

    Create Tipo AB

---
+++ E-Food/E-Food/Models/MantenimientoTipoAB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+namespace E_Food.Models
+{
+    public class MantenimientoTipoAB
+    {
+        SqlCommand cmd = new SqlCommand(
+        "Ten Most Expensive Products", Conexion.getConnection());
+
+        // 2. set the command object so it knows
+        // to execute a stored procedure
+        cmd.CommandType = CommandType.StoredProcedure;
+        
+    }
+}
