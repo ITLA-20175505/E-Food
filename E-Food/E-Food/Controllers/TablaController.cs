3429b06bb9962ecc207215e0bf89276919ead16b
Freddysotof <freddy.sotof23@hotmail.com>
2019-08-08 10:44:11 -0400

    k

--- E-Food/E-Food/Controllers/TablaController.cs
+++ E-Food/E-Food/Controllers/TablaController.cs
@@ -13,45 +13,9 @@ namespace E_Food.Controllers
         // GET: Tabla
         public ActionResult Index()
         {
-            List<ListaTabla> listaTipoAB;
-            using(EFood bd = new EFood())
-            {
-               listaTipoAB = (from d in bd.TipoABs
-                                select new ListaTabla
-                                {
-                                    idTipoAB = d.idTipo,
-                                    Nombre = d.Nombre
-                                }).ToList();
-            }
-            return View(listaTipoAB);
+          
+                       return View();
         }
-        public ActionResult Nuevo()
-        {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult Nuevo(TablaTipoAB model)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    using(EFood bd = new EFood())
-                    {
-                        var tabla = new TipoAB();
-                        tabla.Nombre = model.Nombre;
-
-                        bd.TipoABs.Add(tabla);
-                        bd.SaveChanges();
-                    }
-                    return Redirect("/");
-                }
-                return View(model);
-            }
-            catch(Exception e)
-            {
-                throw new Exception(e.Message);
-            }
-        }
+       
         }
     }
