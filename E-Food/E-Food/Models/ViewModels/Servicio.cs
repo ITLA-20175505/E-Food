5ab881ce94ae05f1407b56b252b7f5183df036dd
Yeduin <el_yeduin_02@hotmail.com>
2019-08-08 16:48:10 -0400

    View de Listar Ordenes

--- E-Food/E-Food/Models/ViewModels/Servicio.cs
+++ E-Food/E-Food/Models/ViewModels/Servicio.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.ComponentModel.DataAnnotations
+using System.ComponentModel.DataAnnotations;
 
 namespace E_Food.Models.ViewModels
 {
